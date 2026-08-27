c206fcba3e30abab2889ffcc4a0111750ea5aca4
MShoaibSadiq845 <shoaib_sadiq_intern@netixsol.com>
2026-08-27 09:29:23 -0700

    change id

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -40,8 +40,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('set', 'autoConfig', false, '2156501118602442');
-              fbq('init', '2156501118602442');
+              fbq('init', '994425453654094');
               fbq('track', 'PageView');
             `,
           }}
@@ -51,7 +50,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=2156501118602442&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=994425453654094&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
