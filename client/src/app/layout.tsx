caff65859af4383f639b1516345d96c536264b0d
MShoaibSadiq845 <shoaib_sadiq_intern@netixsol.com>
2026-09-10 05:31:17 -0700

    correct name

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -6,7 +6,7 @@ import { LoadingProvider } from "@/context/LoadingContext"; // 👈 1. Import Lo
 import Script from "next/script";
 
 export const metadata: Metadata = {
-  title: "FabDecor | E-Commerce Platform & Admin Dashboard",
+  title: "FabDecor | E-Commerce Platform",
   description: "Pixel-perfect e-commerce platform with dynamic RTK Query API layer, NestJS backend, MongoDB, and real-time Socket.IO sales alerts.",
   icons: {
     icon: [
